432eb6c9fe2ac9c9a19b3f8e720d577eccf8dacf
Zain Ali Asif <zainaliasif@hotmail.com>
2015-03-14 18:24:37 -0400

    Relay library

---
+++ Relay.cpp
@@ -0,0 +1,22 @@
+/*
+   Relay.h - Library to control a relay.
+   Created by Zain A. Asif, February 26, 2015.
+*/
+#include "Arduino.h"
+#include "Relay.h"
+
+Relay::Relay(int pin)
+{
+    pinMode(pin, OUTPUT);
+   _pin = pin; 
+}
+
+void Relay::on()
+{
+    digitalWrite(_pin, HIGH);    //Turns Relay on
+}
+
+void Relay::off()
+{
+    digitalWrite(_pin, LOW);    //Turns Relay off    
+}
---
+++ Relay.h
@@ -0,0 +1,21 @@
+/*
+   Relay.h - Library to control a relay.
+   Created by Zain A. Asif, February 26, 2015.
+*/
+
+#ifndef Relay_h
+#define Relay_h
+
+#include "Arduino.h"
+
+class Relay
+{
+  public:
+    Relay (int pin);
+    void on();
+    void off();
+  private:
+    int _pin;
+};
+
+#endif
